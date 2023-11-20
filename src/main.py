aad4f96fb4cdd80a542035f2bb54b0875384e95d
Achique Luis Daniel <achique.luisdan@gmail.com>
2023-11-19 23:10:03 -0400

    dev: 🚀 render de habilidades ocultas del compañero

--- src/main.py
+++ src/main.py
@@ -1,12 +1,73 @@
+from pynput import keyboard
 import random
-
+import os
 
 from data import characters
-from views import output_hidden_skills
+from views import output_hidden_skills, show_partner
 
 quantity = len(characters)
 index = random.randint(1, quantity - 1)
 partner = characters[index]
-print(partner.get('name'))
 
-output_hidden_skills(partner)
+position = 0
+successes = {'.', ','}
+
+
+def render():
+    os.system("cls")
+    print(partner.get('skills'))
+    if is_winner():
+        show_partner(partner)
+    else:
+        output_hidden_skills(partner, position, successes)
+
+
+def increment_position():
+    global position
+    position += 1
+
+
+def decrement_position():
+    global position
+    position -= 1
+    if position < 0:
+        position = 0
+
+
+def is_match(letter):
+    if letter.lower() == partner.get('skills')[position].lower():
+        successes.add(letter)
+
+
+def is_winner() -> bool:
+    skills: str = ''
+    skills = partner.get('skills').replace(' ', '')
+    return len(successes) >= len(skills)
+
+
+def on_press(key):
+    try:
+        is_match(key.char)
+    except AttributeError:
+        pass
+
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+    elif key == keyboard.Key.right:
+        increment_position()
+    elif key == keyboard.Key.left:
+        decrement_position()
+    render()
+
+
+render()
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+
+render()
+listener.start()
--- src/views.py
+++ src/views.py
@@ -1,9 +1,31 @@
 
-def output_hidden_skills(partner: dict):
+def output_hidden_skills(partner: dict, position: int, successes: set):
     skills: str = partner.get('skills')
+    skills_not_space = partner.get('skills').replace(' ', '')
+    print('Aciertos:', len(successes) - 2, '/', len(skills_not_space))
+    if skills[position] != ' ' and not skills[position].lower() in successes:
+        skills = skills.replace(
+            skills[position], '🔤'
+        )
+    if skills[position].lower() in successes:
+        skills = skills.replace(
+            skills[position], '✅'
+        )
     for letter in skills:
-        if letter != ' ':
+        if letter.lower() in successes:
+            print(letter, end="")
+        elif letter == '🔤':
+            print('🔤', end="")
+        elif letter == '✅':
+            print('✅', end="")
+        elif letter != ' ':
             print('_', end="")
         else:
             print(letter, end="")
     print('')
+
+
+def show_partner(partner: dict):
+    print(partner.get('emoji'), print(partner.get('name')))
+    print('\n')
+    print(partner.get('skills'))
